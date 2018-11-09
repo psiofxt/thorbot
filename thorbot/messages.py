044ed8e75799ee14e609b3758a46fbe6af2335a5
Andrew <arellok@gmail.com>
2018-11-08 21:26:50 -0800

    update welcome

--- thorbot/messages.py
+++ thorbot/messages.py
@@ -22,7 +22,7 @@ FINAL_WARNING = (
 )
 
 WELCOME_MESSAGE = (
-    ":headphones: *Welcome {first_name} to Emanate's Telegram!* :headphones:"
+    ":robot: :headphones: *Welcome {first_name} to Emanate's Telegram!* :headphones:"
     "\nPlease take a look at the *pinned post* and check out our website https://emanate.live/. "
     "If you have any questions, don't hesitate to ask one of our friendly admins! "
     "We will be posting new developments as they happen, so stay tuned. *Thanks again for joining us!*"
