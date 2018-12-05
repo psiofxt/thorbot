5950283524c0aa7a5db46fcf1e6e884a95997c40
Andrew <arellok@gmail.com>
2018-12-04 21:53:10 -0800

    Update welcome message

--- thorbot/messages.py
+++ thorbot/messages.py
@@ -22,12 +22,14 @@ FINAL_WARNING = (
 )
 
 WELCOME_MESSAGE = (
-    ":robot_face: Welcome {first_name} to the dbcrypto community Telegram!\n"
-    "Feel free to join in on any conversations and stay tuned in channel for "
-    "community AMA's. Also feel free to head over to the dbcrypto YouTube "
-    "channel to check any recorded AMAs. *Thanks again for joining us!*\n"
-    "[Website](dbcrypto.io)\n"
-    "[YouTube](https://bit.ly/2LTOyHa)"
+    "Welcome {first_name} to the dbcrypto community Telegram!\n"
+    "Feel free to join in on the conversation and read the pinned "
+    "message for more information. Announcement channel can be found at "
+    "t.me/dbcryptoann. *Thanks again for joining us!*\n"
+    "—\n"
+    "dbcrypto.io\n"
+    "twitter.com/dalb0013\n"
+    "youtube.com/c/dbcrypto"
 )
 
 AIRDROP = (
